e83d5454e7ac44a4a9085a8d9e92c9a96ad0ccc9
saidevakumar <saidevakumar@gmail.com>
2023-03-17 13:00:19 -0400

    Added PLINQ & Array examples.
    Added PLINQ & Array examples.

---
+++ Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] ifixedarray = new int[4];
+Console.WriteLine(String.Format("The length of the ifixedarray = {0} ",ifixedarray.Length));
+
+int[] ifixedassignedArray = new int[]{1,2,3,4};
+Console.WriteLine(String.Format("The length of the ifixedassignedArray = {0} ",ifixedassignedArray.Length));
+
+for(int idx = 0; idx < ifixedassignedArray.Length; idx++)
+{
+    Console.WriteLine(ifixedassignedArray[idx]);
+}
---
+++ PLINQ/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var source = Enumerable.Range(1, 10000);
+
+// Opt in to PLINQ with AsParallel.
+var evenNums = from num in source.AsParallel()
+    where num % 2 == 0
+    select num;
+
+Console.WriteLine("{0} even numbers out of {1} total",evenNums.Count(), source.Count());
+
